36c54771941e33e63bd36d077de0a049f251ea1a
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-07-01 00:20:26 +0900

    パスフレーズとOpenSSH表記になった

--- python/RSA_Generate.py
+++ python/RSA_Generate.py
@@ -1,14 +1,20 @@
 from Crypto.PublicKey import RSA
 
+Buffer = 2048
+
+input_value = input("Buffer (default is 2048):")
+if input_value != "":
+    Buffer = int(input_value)
+
 # 秘密鍵の作成
-key = RSA.generate(2048)
-private_key = key.export_key()
+key = RSA.generate(Buffer)
+private_key = key.export_key(format="PEM", passphrase="passphrase", protection="DER")
 file_out = open("private.pem", "wb")
 file_out.write(private_key)
 file_out.close()
 
 # 公開鍵の作成
-public_key = key.publickey().export_key()
+public_key = key.publickey().export_key(format="OpenSSH")
 file_out = open("public.pem", "wb")
 file_out.write(public_key)
 file_out.close()
