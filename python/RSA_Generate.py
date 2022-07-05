7efe361e80709124c8ef380a48fe55cb08cb9259
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-07-06 00:25:03 +0900

    passphrase and savepath and buffer input

--- python/RSA_Generate.py
+++ python/RSA_Generate.py
@@ -1,20 +1,35 @@
 from Crypto.PublicKey import RSA
-from os.path import expanduser
+from os import path
 from getpass import getpass
 
-UserRoot = expanduser("~")
+userRoot = path.expanduser("~")
+
 Buffer = 2048
+savepath = userRoot + "\.ssh\id_rsa"
 passphrase = ""
 
-input_value = input("Buffer (default is 2048):")
-if input_value:
-    Buffer = int(input_value)
+input_value = input("Buffer size (default is 2048):")   # バッファサイズ
+if input_value: # 空白時デフォルト使用
+    if input_value.isdecimal():     #数値に変換できる場合は変更
+        Buffer = int(input_value)   # +整数のみ変換
+    else:
+        print("Error: Buffer size is bad value")
+        quit(0)
+
+input_savepath = input(f"save key path ({savepath}):")  # 保存場所
+if input_savepath:  # 空白時デフォルト使用
+    if path.isdir(input_savepath.rstrip(input_savepath.split("\\")[-1])):   # user\.ssh\id_rsa のid_rsa部分の切り取り
+        savepath = input_savepath                                           # フォルダー確認に保存けってい
+    else:
+        print(f"Error: not found folder {input_savepath}")
+        quit(0)
 
 while True:
-    input_pass = getpass("passphrase (empty for no passphrase):")
+    input_pass = getpass("passphrase (empty for no passphrase):")   # パスフレーズ  getpassでコンソールに表示されない
+
     if not input_pass:  # 空白なら抜ける
         break
-    input_pass_sec = getpass("passphrase again:")
+    input_pass_sec = getpass("passphrase again:")   # 確認用に２回入力
     if input_pass == input_pass_sec:
         passphrase = input_pass
         break
@@ -23,14 +38,17 @@ while True:
 key = RSA.generate(Buffer)
 
 print("Generate Private Key...")
-private_key = key.export_key(format="PEM", passphrase=passphrase, pkcs=1 , protection="DER")
-file_out = open(f"{UserRoot}\id_rsa", "wb")
+if passphrase:
+    private_key = key.export_key(format="PEM", passphrase=passphrase, protection="DER")
+else:
+    private_key = key.export_key(format="PEM")
+file_out = open(f"{savepath}", "wb")
 file_out.write(private_key)
 file_out.close()
 
 # 公開鍵の作成
 print("Generate Public Key...")
 public_key = key.publickey().export_key(format="OpenSSH")
-file_out = open(f"{UserRoot}\id_rsa.pub", "wb")
+file_out = open(f"{savepath}.pub", "wb")
 file_out.write(public_key)
 file_out.close()
