4c9267cb3ce395852a3dcd4fcdc5a8d1a1223e0d
pantyetta <62027884+pantyetta@users.noreply.github.com>
2022-07-07 15:56:39 +0900

    関数化して機能分散 & エラー調整

--- python/RSA_Generate.py
+++ python/RSA_Generate.py
@@ -2,53 +2,78 @@ from Crypto.PublicKey import RSA
 from os import path
 from getpass import getpass
 
-userRoot = path.expanduser("~")
 
-Buffer = 2048
-savepath = userRoot + "\.ssh\id_rsa"
-passphrase = ""
+def getBufferSize(default=2048):
+    input_value = input("Buffer size (default is 2048):")  # バッファサイズ
+    if input_value:  # 空白時デフォルト使用
+        if input_value.isdecimal():  # 数値に変換できる場合は変更
+            return int(input_value)  # +整数のみ変換
+        else:
+            print("Error: Buffer size is bad value")
+            quit(0)
+    return default
 
-input_value = input("Buffer size (default is 2048):")   # バッファサイズ
-if input_value: # 空白時デフォルト使用
-    if input_value.isdecimal():     #数値に変換できる場合は変更
-        Buffer = int(input_value)   # +整数のみ変換
+
+def getPath(default=path.expanduser("~") + r"\.ssh\id_rsa"):
+    input_savepath = input(f"save key path ({default}):")  # 保存場所
+    if input_savepath:  # 空白時デフォルト使用
+        if path.isdir(input_savepath.rstrip(input_savepath.split("\\")[-1])):  # user\.ssh\id_rsa のid_rsa部分の切り取り
+            return input_savepath  # フォルダー確認に保存けってい
+        else:
+            print(f"Error: not found folder {input_savepath}")
+            quit(0)
     else:
-        print("Error: Buffer size is bad value")
-        quit(0)
+        if path.isdir(default.rstrip(default.split("\\")[-1])):  # user\.ssh\id_rsa のid_rsa部分の切り取り
+            return default  # フォルダー確認に保存けってい
+        else:
+            print(f"Error: not found folder {input_savepath}")
+            quit(0)
 
-input_savepath = input(f"save key path ({savepath}):")  # 保存場所
-if input_savepath:  # 空白時デフォルト使用
-    if path.isdir(input_savepath.rstrip(input_savepath.split("\\")[-1])):   # user\.ssh\id_rsa のid_rsa部分の切り取り
-        savepath = input_savepath                                           # フォルダー確認に保存けってい
+
+def getPassphrase():
+    passphrase = ""
+    while True:
+        input_pass = getpass("passphrase (empty for no passphrase):")  # パスフレーズ  getpassでコンソールに表示されない
+        if not input_pass:  # 空白なら抜ける
+            break
+
+        input_pass_sec = getpass("passphrase again:")  # 確認用に２回入力
+        if input_pass == input_pass_sec:
+            passphrase = input_pass
+            break
+    return passphrase
+
+
+def saveFile(rsa_key, file_path, mode=0):
+    if mode == 0:
+        fw = open(f"{file_path}", "wb")
+    elif mode == 1:
+        fw = open(f"{file_path}.pub", "wb")
     else:
-        print(f"Error: not found folder {input_savepath}")
-        quit(0)
+        raise Exception("mode is out of range")
+    fw.write(rsa_key)
+    fw.close()
 
-while True:
-    input_pass = getpass("passphrase (empty for no passphrase):")   # パスフレーズ  getpassでコンソールに表示されない
 
-    if not input_pass:  # 空白なら抜ける
-        break
-    input_pass_sec = getpass("passphrase again:")   # 確認用に２回入力
-    if input_pass == input_pass_sec:
-        passphrase = input_pass
-        break
+def main():
 
-# 秘密鍵の作成
-key = RSA.generate(Buffer)
+    print("Generate Private Key...")
+    # 秘密鍵の作成
+    key = RSA.generate(getBufferSize())
+    savepath = getPath()
+    passphrase = getPassphrase()
 
-print("Generate Private Key...")
-if passphrase:
-    private_key = key.export_key(format="PEM", passphrase=passphrase, protection="DER")
-else:
-    private_key = key.export_key(format="PEM")
-file_out = open(f"{savepath}", "wb")
-file_out.write(private_key)
-file_out.close()
+    if passphrase:
+        private_key = key.export_key(format="PEM", passphrase=passphrase, protection="DER")
+    else:
+        private_key = key.export_key(format="PEM")
+    saveFile(private_key, savepath, 0)
 
-# 公開鍵の作成
-print("Generate Public Key...")
-public_key = key.publickey().export_key(format="OpenSSH")
-file_out = open(f"{savepath}.pub", "wb")
-file_out.write(public_key)
-file_out.close()
+    # 公開鍵の作成
+    print("Generate Public Key...")
+    public_key = key.publickey().export_key(format="OpenSSH", protection="DER")
+    saveFile(public_key, savepath, 1)
+
+
+if __name__ == '__main__':
+    main()
